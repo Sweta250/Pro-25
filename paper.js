8400f4cf84de202709baebe50cb778273a4d7d9f
Sweta250 <74644394+Sweta250@users.noreply.github.com>
2021-01-27 22:54:28 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class Paper{
+    constructor(x,y,radius){
+        var bp={
+            isStatic: false,
+            restitution:1.0,
+            friction:0.5,
+            density:1.2
+        }
+        this.radius=radius
+       // this.image=loadImage("paper.png");
+        this.body =Bodies.circle(x,y,radius,bp);
+World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        //var angle= this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        //imageMode (CENTER)
+       //image(this.image, 0,-40, this.width, this.height);
+         //angleMode(RADIANS);
+        //rotate(angle);
+        ellipseMode(RADIUS)
+        fill("purple"); 
+  circle(0,0,this.radius);
+    pop();
+}
+}
